b3ecacfdc4678b652a9f4e54d06afa5d2b931238
kjkep <162730987+kjkep@users.noreply.github.com>
2024-04-05 17:01:37 -0400

    Update Group.java

--- Group.java
+++ Group.java
@@ -4,6 +4,20 @@ public class Group {
 	String message= "Anthony Smith";
 	return message;
    	}
+
+	public String kaylaKeplinger() {
+		return "Kayla Keplinger";
+	}
+
+	// please add your own names :)
+	public String[] getGroupMembers() {
+		String names[] = new String[5];
+		
+		names[0] = kaylaKeplinger();
+		
+		return names;
+	}
+	
 	double Multiply (int num1,int num2)
   	{
   		int result=0;
@@ -13,6 +27,16 @@ public class Group {
   		return result;
   	}
 	public static void main(String[] args) {
-		System.out.println(getAnthonysName));
+		System.out.println(getAnthonysName);
+
+		// Kayla's addition
+		Group g = new Group();
+		String names[] = g.getGroupMembers();
+		
+		for (String name : names) {
+			System.out.println(name);
+		}
 	}
 }
+
+
