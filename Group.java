cd9a94ae7f91460ed30368f580bae071b941c6cc
AnthonyS1234 <163884235+AnthonyS1234@users.noreply.github.com>
2024-04-05 16:52:34 -0400

    Update Group.java

--- Group.java
+++ Group.java
@@ -1,6 +1,18 @@
 
 public class Group {
+	static String getAnthonysName() {
+	String message= "Anthony Smith";
+	return message;
+   	}
+	double Multiply (int num1,int num2)
+  	{
+  		int result=0;
+  			for(int i =0; i < num1;i++) {
+  			result=result+num2;
+  			}
+  		return result;
+  	}
 	public static void main(String[] args) {
-		
+		System.out.println(getAnthonysName));
 	}
 }
