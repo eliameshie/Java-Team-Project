80608ad1a3af08796d54d6bc9ad463a9bf6fa2b2
AnthonyS1234 <163884235+AnthonyS1234@users.noreply.github.com>
2024-04-05 17:33:39 -0400

    Update Group.java

--- Group.java
+++ Group.java
@@ -4,8 +4,25 @@ public class Group {
 	String message= "Anthony Smith";
 	return message;
    	}
-	double Multiply (int num1,int num2)
-  	{
+	int power(int num1, int num2){
+		if(num2==0){
+			return 1;
+		}
+		int result = num1;
+		 for(int i=1; i < num2;i++){
+			 int tmp=0;
+			 for(j=0;i < num1;i++){
+				tmp+=result;
+			 }
+			 result=tmp;
+		 }
+			 
+		
+		return result;
+	}
+		
+	
+	double Multiply (int num1,int num2){
   		int result=0;
   			for(int i =0; i < num1;i++) {
   			result=result+num2;
