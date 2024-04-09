96db47126125d7789c0bd5e8f5fb422c2077a7a8
AnthonyS1234 <163884235+AnthonyS1234@users.noreply.github.com>
2024-04-09 11:02:08 -0400

    Update UnitTests.java

--- src/main/java/UnitTests.java
+++ src/main/java/UnitTests.java
@@ -6,18 +6,18 @@ public class TestCases{
    @Test
 	public void Power() {
 		CodeToTest tst1 = new CodeToTest();
-		double output_mul=tst2.divide(5, 2);
+		double output_mul=tst2.power(5, 2);
 		assertEquals(25,output_mul,0.01);
 		} 
   @Test
 	public void multiply() {
 		CodeToTest tst2 = new CodeToTest();
-		double output_mul=tst2.divide(10, 2);
+		double output_mul=tst2.Multiply(10, 2);
 		assertEquals(20,output_mul,0.01);
 		}
   @Test
 	public void intDivision() {
 		CodeToTest tst3 = new CodeToTest();
-		double output_div=tst2.divide(10, 2);
+		double output_div=tst2.Divide(10, 2);
 		assertEquals(5,output_div,0.01);
 		}
